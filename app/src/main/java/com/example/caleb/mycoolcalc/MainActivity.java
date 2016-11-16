7b9eb4959e2a5a998a425c9967b44952a508d3e7
Caleb Garrick <caleb.garrick@mail.lipscomb.edu>
2016-11-15 19:48:48 -0500

    Defined number functions

--- app/src/main/java/com/example/caleb/mycoolcalc/MainActivity.java
+++ app/src/main/java/com/example/caleb/mycoolcalc/MainActivity.java
@@ -14,22 +14,53 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void buttonClear(View view) {
-        TextView result=(TextView) findViewById(R.id.result);
+        TextView result = (TextView) findViewById(R.id.result);
         result.setText("0");
     }
 
+    public void buttonOne(View view) {
+        TextView result = (TextView) findViewById(R.id.result);
+        result.setText(result + "1");
+    }
+
+    public void buttonTwo(View view) {
+        TextView result = (TextView) findViewById(R.id.result);
+        result.setText(result + "2");
+    }
+
+    public void buttonThree(View view) {
+        TextView result = (TextView) findViewById(R.id.result);
+        result.setText(result + "3");
+    }
+
+    public void buttonFour(View view) {
+        TextView result = (TextView) findViewById(R.id.result);
+        result.setText(result + "4");
+    }
+
+    public void buttonFive(View view) {
+        TextView result = (TextView) findViewById(R.id.result);
+        result.setText(result + "5");
+    }
+
+    public void buttonSix(View view) {
+        TextView result = (TextView) findViewById(R.id.result);
+        result.setText(result + "6");
+    }
+
     public void buttonSeven(View view) {
-        TextView result=(TextView) findViewById(R.id.result);
-        result.setText("7");
+        TextView result = (TextView) findViewById(R.id.result);
+        result.setText(result + "7");
     }
 
     public void buttonEight(View view) {
-        TextView result=(TextView) findViewById(R.id.result);
-        result.setText("8");
+        TextView result = (TextView) findViewById(R.id.result);
+        result.setText(result + "8");
     }
 
+
     public void buttonNine(View view) {
-        TextView result=(TextView) findViewById(R.id.result);
-        result.setText("9");
+        TextView result = (TextView) findViewById(R.id.result);
+        result.setText(result + "9");
     }
 }
